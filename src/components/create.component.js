d789ece1736d823fdb70da4579ddc04b7252df4b
soc33 <soc33@msn.com>
2019-06-06 19:45:52 -0400

    basic components

---
+++ src/components/create.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Create extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Add a book page</h1>
+      </div>
+    )
+  }
+}
